c9b96c15075585f91f4a46c5f79a1875c98bddc0
Biootak <bioootak@gmail.com>
2024-08-25 22:54:41 +0330

    feat(next-auth): add new type definitions for user role

---
+++ next-auth.d.ts
@@ -0,0 +1,15 @@
+import type { Role } from '@/types/types';
+import { DefaultSession } from 'next-auth'
+import { JWT } from "next-auth/jwt"
+
+export type ExtendUser = DefaultSession['user'] & {
+    role: Role
+}
+
+
+
+declare module 'next-auth' {
+    interface Session {
+        user: ExtendUser
+    }
+}
