a8d633a5f47ac8eee983e4ca53d92198f9102fde
Biootak <bioootak@gmail.com>
2024-08-27 16:55:05 +0330

    feat(SectionLargeSlider): Add loading skeleton for featured posts slider

--- src/app/(site)/(home)/SectionLargeSlider.tsx
+++ src/app/(site)/(home)/SectionLargeSlider.tsx
@@ -3,19 +3,24 @@ import { getFeaturedPosts } from '@/actions/getFeaturedPosts';
 
 import type { PostWithRelations, ActionResult } from '@/types/types';
 import SectionLargeSliderClient from './SectionLargeSliderClient';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const getFeaturedPostsCached = cache(getFeaturedPosts);
 
 export default async function SectionLargeSlider() {
   const result: ActionResult<PostWithRelations[]> = await getFeaturedPostsCached(3);
 
-  
-
   return (
-    <SectionLargeSliderClient
-      initialPosts={result.data }
-      autoSlide={true}
-      className="pt-10 pb-16 md:py-16 lg:pb-28 lg:pt-5"
-    />
+    <div>
+      {result.data ? (
+        <SectionLargeSliderClient
+          initialPosts={result.data}
+          autoSlide={true}
+          className="pt-10 pb-16 md:py-16 lg:pb-28 lg:pt-5"
+        />
+      ) : (
+        <Skeleton className="h-[500px] w-full" />
+      )}
+    </div>
   );
 }
