7457ea5338710dc3d9d578bfa88a16a24abde0b4
Biootak <bioootak@gmail.com>
2024-09-19 18:41:21 +0330

    Update Next.js config: disabled reactStrictMode, optimizeCss and scrollRestoration.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   poweredByHeader: false,
   typescript: {
     ignoreBuildErrors: false,
@@ -46,8 +46,8 @@ const nextConfig = {
       bodySizeLimit: '3mb',
     },
 
-    optimizeCss: true,
-    scrollRestoration: true,
+    // optimizeCss: true,
+    // scrollRestoration: true,
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
