b6f5497a2c85737fb7ca5730940f55de7c67a321
Biootak <bioootak@gmail.com>
2024-10-05 14:53:12 +0330

    "Update next config: remove standalone output, add minimumCacheTTL."

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  output: 'standalone',
+
   swcMinify: true,
   compress: true,
   productionBrowserSourceMaps: false,
@@ -47,6 +47,7 @@ const nextConfig = {
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
   },
 
   experimental: {
